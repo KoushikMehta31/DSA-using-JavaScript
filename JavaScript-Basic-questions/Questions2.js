f51fac37b47ea093ce0be7745f4d10738190e604
Koushik Mehta <koushikmehta07@gmail.com>
2026-06-15 22:26:21 +0530

    Create Questions2.js

---
+++ JavaScript-Basic-questions/Questions2.js
@@ -0,0 +1,14 @@
+// add two number using (Arithmetic & function)
+
+let a = 10;
+let b = 20;
+let result = a+b;
+console.log(result);
+
+
+
+function add(a,b){
+  let result = a+b;
+  console.log(result);
+}
+add(10,30);

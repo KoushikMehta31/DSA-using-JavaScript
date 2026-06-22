3f891c3302e22842c55ab037a534b217db3915d7
Koushik Mehta <koushikmehta07@gmail.com>
2026-06-22 20:29:26 +0530

    Add search functionality for an element in an array

--- Array-questions/codechef-questions/Search-an-element-in-an-array.js
+++ Array-questions/codechef-questions/Search-an-element-in-an-array.js
@@ -7,3 +7,18 @@ function searchElement(N,X,A) {
     }
     return "NO";
 }
+
+_____________________________________________________
+
+
+let a = [7, 3, 5, 2, 1];
+let x = 3;
+let n = a.length;
+
+for(let i=0; i<n; i++){
+    if(a[i] === x){
+        console.log("YES");
+        return;
+    }
+}
+console.log("NO");

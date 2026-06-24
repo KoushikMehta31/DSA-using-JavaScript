73b2d2ace6799c52b8be46f76fdbf8869a7e645a
Koushik Mehta <koushikmehta07@gmail.com>
2026-06-24 22:53:33 +0530

    Update Consecutive-Ones.js

--- Array-questions/codechef-questions/Consecutive-Ones.js
+++ Array-questions/codechef-questions/Consecutive-Ones.js
@@ -14,6 +14,28 @@ for(let i=0; i<arr.length; i++){
 }
 console.log(maxcount);
 
-
-
 answer is 3
+
+_______________________________________________________________________________
+
+
+function findMaxConsecutiveOnes(nums) {
+    let maxcount = 0;
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] === 1) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if(count > maxcount) {
+            maxcount = count;
+        }
+    }
+
+    return maxcount;
+}
+
+console.log(findMaxConsecutiveOnes([1,1,1,0,0,1,1])); // 3

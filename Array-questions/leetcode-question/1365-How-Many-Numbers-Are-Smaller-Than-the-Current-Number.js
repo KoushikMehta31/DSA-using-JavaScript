6bcddfc936ca95a2077c197a2759eeda9ab5ce3a
Koushik Mehta <koushikmehta07@gmail.com>
2026-07-03 13:41:39 +0530

    Create 1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js

---
+++ Array-questions/leetcode-question/1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js
@@ -0,0 +1,14 @@
+
+var smallerNumbersThanCurrent = function(nums) {
+    let ans = [];
+    for(let i=0; i<nums.length; i++){
+        let count = 0;
+        for(let j=0; j<nums.length; j++){
+            if(nums[j] < nums[i]){
+                count++;
+            }
+        }
+        ans.push(count);
+    }
+    return ans;
+};
